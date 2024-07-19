5a6ede98cb32ecd225b64cbca374e73b0de63bee
Eli Maxwell-Aidam <emaxwellaidam@gmail.com>
2024-07-19 17:05:56 +0000

    Addition of Logo to Style the application

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -6,6 +6,7 @@ import { IoExitOutline } from "react-icons/io5";
 import { IoDocumentTextSharp } from "react-icons/io5";
 import Button from 'react-bootstrap/Button';
 import "./css/SideBar.css"
+import Logo from "../assets/blueLogo.png"
 const SideBar = () => {
     const navigate = useNavigate()
 
@@ -17,7 +18,12 @@ const SideBar = () => {
   return (
     <div style={{backgroundColor:"#242627"}} className='w-25 vh-100 ps-4 pe-4 pt-2 d-flex flex-column' id='sidebar'>
 
-            <div className='header d-flex justify-content-center pt-4 pb-3 border-bottom'><h3>Site Checker</h3></div>
+            <div className='header d-flex justify-content-center align-items-center pt-4 pb-3 border-bottom'>
+                <img src={Logo} alt="" style={{width:"auto", height:"3.5rem", paddingBottom:"0.2rem"}}/>
+
+            <h3>Site Checker</h3>
+
+            </div>
 
            
                 <Nav className="flex-column align-items-center gap-5 pt-5 pb-5 vh-100" style={{fontSize:"1.3rem"}}>
